9d9e02c9233154b2397067fedd344c028a21a28e
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-11 05:34:33 +0530

    Add files via upload

---
+++ C9_TA1_Template.py
@@ -0,0 +1,15 @@
+# Import pygame
+
+# Initialize pygame
+ 
+# Create a game screen with width=600 and height=600 and name is as "screen"
+
+# Set the game title as "Car Racing Game"
+
+# Game loop
+
+
+
+
+ 
+# Quit the game 
